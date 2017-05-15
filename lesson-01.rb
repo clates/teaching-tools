baffadf9abd9514a71d06b1f2fe7e0896a0c4560
clates <ChristopherLates@gmail.com>
2017-05-15 17:58:19 -0400

    post-lesson1

--- lesson-01.rb
+++ lesson-01.rb
@@ -19,22 +19,25 @@ arr = []
 
 # arr << "Ashley"
 # ap "We put a string into the first position of the array and printed it below. Arrays are indexed starting at zero."
-# ap arr
+# print arr
 # ap "*"*25
 
 
+arr << "Ashley"
+arr << 85
+# arr << {numberInObject:12}
+# ap "We added a few other elements to the array using the << (push) notation. Note that Ruby doesn't mind having many different object types in an individual array."
+# print arr
+# puts
+# print arr[1]
+# puts
+# ap "*"*25
+
+# ap "Play with the following array commands and see what they do to the array"
+# #init contents of the array
 # arr << "Ashley"
 # arr << 85
 # arr << {numberInObject:12}
-# ap "We added a few other elements to the array using the << (push) notation. Note that Ruby doesn't mind having many different object types in an individual array."
-# ap arr
-# ap "*"*25
-
-ap "Play with the following array commands and see what they do to the array"
-#init contents of the array
-arr << "Ashley"
-arr << 85
-arr << {numberInObject:12}
 #array commands below
 # arr << element #Push / Append an element onto the end of the array
 # arr.push(element) #Same as previous
@@ -42,15 +45,19 @@ arr << {numberInObject:12}
 # arr.clear #Erase all elements in arr.
 # arr.first #Returns the first element in arr
 # arr.last #Returns the last element in arr
-# arr.sample #Returns a random element in arr
+# puts arr.sample #Returns a random element in arr
 ## more at https://www.shortcutfoo.com/app/dojos/ruby-arrays/cheatsheet
 
 
 #Strings
 # Strings are best described as anything inside of quotes. These are usually characters, words, or sentences that have
 # no intention of math ever occurring on them.
-# firstName = "Ashley"
+# firstName = 'Ashley "the knucklhead"\'s suitcase '
 # lastName = 'McGoober'
+# otherLastName = 'Mcgoober'
+
+# puts "lastName:#{lastName.upcase} is equal to otherLastName:#{otherLastName.upcase}"
+# puts lastName.upcase == otherLastName.upcase
 # Both single quote (') and double quote (") are allowed to define strings in Ruby. Strings share MANY similarities to Arrays,
 # and it makes sense if you consider that a string is really just an array of single characters.
 # ['A', 's', 'h', 'l', 'e', 'y']
@@ -59,6 +66,11 @@ arr << {numberInObject:12}
 #Integers
 # Integers are whole numbers and are not surrounded by quotation marks.
 # currentYear = 2017
+# currentYear_s = '2017'
+
+# puts 4 * 4 
+# puts '4' * 4
+# puts '4' * '4'
 # Integers work intuitively for the most part
 # nextYear = currentYear + 1
 
@@ -66,6 +78,18 @@ arr << {numberInObject:12}
 
 #Hashes / Dictionary / Object
 
+employee = {
+        :name => "Ashley",
+        :gender => "Female",
+        :age => 26,
+        :skills => ["Ruby", "Gardening", "Nose-picking"]
+    }
+
+puts employee
+
+# employee[:retirement] = "401k"
+
+print employee.keys
 
 ##############################
 #Other less often used types
