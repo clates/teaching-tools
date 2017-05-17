f678f3868cb65d9a5317466078fc7a1af2fe0f03
clates <christopherlates@gmail.com>
2017-05-17 14:00:34 -0400

    Some more lesson 03

--- lesson-03.rb
+++ lesson-03.rb
@@ -37,13 +37,52 @@ end
 woernerFamily = ["Ashley", "Rachael", "Gail", "Austin", "Jason", "Geoff"]
 
 woernerFamily.each{|name|
-  if name.length > 5
-    puts "#{x} is longer than 5 characters"
+  if name.length > 5 then
+    puts "#{name} is longer than 5 characters"
   end
 }
 
 woernerFamily.each_with_index{|name, index|
-  if name.length > 5
-    puts "#{x} is longer than 5 characters and is located at index #{index}"
+  if name.length > 5 then
+    puts "#{name} is longer than 5 characters and is located at index #{index}"
   end
 }
+
+#Break, next ....
+# These flow-control commands should *GENERALLY* be avoided. They are a smell and most of the time when
+# they are used they can be refactored to not need them.
+
+#Break , yields the block in question.
+someArrOfNumbers = [1, 2, 4, 2, 0, 2, 4, 2, 2, 4, 2, 2, 4, 2, 2, 4, 2, 2, 4, 2, 2, 4, 2, 2, 4, 2]
+lowestValue = 100
+someArrOfNumbers.each{|x|
+  if lowestValue > x then
+    puts "Found a new lowest value! #{x}"
+    lowestValue = x
+  end
+  if lowestValue == 0
+    puts "We've found a 0. Exiting the loop"
+    break
+  end
+}
+
+#Next , continues to the next iteratable.
+woernerFamilyObj = [{:name => "Ashley", :age => 26, :gender => "female"},
+                    {:name => "Gail", :age => 48, :gender => "female"},
+                    {:name => "Austin", :age => 20, :gender => "male"},
+                    {:name => "Rachael", :age => 31, :gender => "female"},
+                    {:name => "Jason", :age => 20, :gender => "male"},
+                    {:name => "Geoff", :age => 50, :gender => "male"},]
+maleWoerners = []
+woernerFamilyObj.each{|x|
+  if x[:gender] == "female" then
+    puts "#{x[:name]} is not a male and will not be added to the maleWoerner list."
+    next
+  end
+
+  maleWoerners << x
+}
+
+puts "Male Woerners:"
+print maleWoerners
+puts
